e4eded1287e990f4b6263260cf2833ae46cb1223
Mortada Al - Jazaery <97363701+7codeiq@users.noreply.github.com>
2022-07-22 02:09:54 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function mobileMenu() {
+    document.getElementById("mobile").classList.toggle("active");
+    document.getElementById("1").classList.toggle("up-line-active");
+    document.getElementById("2").classList.toggle("line-active");
+    document.getElementById("3").classList.toggle("down-line-active");
+};
